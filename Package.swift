fb2fb823073dadd2045cd6a7d325481e03e1f707
Jason Lew-Rapai <jasonjrr@users.noreply.github.com>
2024-06-07 11:43:49 -0700

    Add MacOS 12 support

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "BusyIndicator",
     platforms: [
         .iOS(.v14),
+        .macOS(.v12),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
